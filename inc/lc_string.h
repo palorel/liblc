8d260a50ddffb830304ca4adbe27794f82874547
Honor Honor <pgl2712spb@gmail.com>
2021-03-16 14:23:13 +0000

    functions

--- inc/lc_string.h
+++ inc/lc_string.h
@@ -1,18 +1,108 @@
 #ifndef FT_STRING_H
 #define FT_STRING_H
 
-size_t		lc_strlen(const char *str);
-size_t		lc_strnlen(const char *s, size_t maxlen);
+size_t		lc_strlen(const char *str)
+{
+    size_t l = 0;
 
-char		*lc_strchr(const char *s, int c);
-char		*lc_strrchr(const char *s, int c);
+    while(*str)
+    {
+        ++l;
+        ++str;
+    }
+    return (l);
+}
+size_t		lc_strnlen(const char *s, size_t maxlen)
+{
+    size_t l = 0;
 
-char		*lc_strstr(const char *str, const char *needle);
-char		*lc_strnstr(const char *s, const char *needle, size_t n);
-char		**lc_strstrs(const char **strs, const char *needle);
+    while(*s)
+    {
+        ++l;
+        ++s;    
+    }
+    return(maxlen > l ? l : maxlen);
+}
 
-int			lc_strcmp(const char *s1, const char *s2);
-int			lc_strncmp(const char *s1, const char *s2, size_t n);
+char		*lc_strchr(const char *s, int c)
+{
+    while(*s)
+    {
+        if (*s == c)
+            return(s);
+        ++s;
+    }
+    return (0);
+}
+
+char		*lc_strrchr(const char *s, int c)
+{
+    char l = 0;
+
+    while(*s)
+    {
+        if (*s == c)
+            l = s;
+        ++s;
+    }   
+    return (l);
+}
+
+char		*lc_strstr(const char *str, const char *needle)
+{
+    int k = 0;
+    int l = 0;
+    char needle_2;
+
+    needle_2 = needle;
+    k = lc_strlen(needle);
+    while (*str)
+    {
+        if (*str == *needle)
+        {
+            if (k == l)
+                return (s);
+            else
+            {
+                ++l;
+                ++needle;
+                ++str;
+            }
+        }
+        ++str;
+        needle = needle_2;
+        l = 0;
+    }
+    return (0);
+}
+char		*lc_strnstr(const char *s, const char *needle, size_t n)
+{
+
+}
+char		**lc_strstrs(const char **strs, const char *needle)
+{
+    
+}
+
+int			lc_strcmp(const char *s1, const char *s2){
+    int k = 0;
+    int l = 0;
+
+    while (*s1 && *s2)
+    {
+        if (*s1)
+            ++l;
+        if (*s2)
+            ++k;
+        ++s1;
+        ++s2;
+    }
+    return(l - k)
+}
+int			lc_strncmp(const char *s1, const char *s2, size_t n)
+{
+    //Функция strncmp() работает аналогичным образом, но сравнивает только первые n символов строки s1.  Что под этим имеется ввиду?
+}
 
 char		*lc_strcpy(char *dest, const char *src);
 char		*lc_strncpy(char *dest, const char *src, size_t n);
